a6614b7ced5913baea3546b05d290d009bb5d9ca
nkizito92 <nkizito57@gmail.com>
2022-02-08 20:25:15 -0600

    style has been updated for chatMessage component

---
+++ components/chatMessages/styles.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        padding: 10,
+        backgroundColor: "#2f285f",
+        borderBottomWidth: 1,
+        borderColor: '#0a052a',
+    },
+    rightContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    name: {
+        fontSize: 18,
+        color: '#ffff',
+        fontWeight: 'bold'
+    },
+    text: {
+        color: "#fffa",
+        fontSize: 16
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    image: {
+        height: 50,
+        width: 50,
+        borderRadius: 20,
+        marginRight: 10
+    },
+    badgeContainer: {
+        position: 'absolute',
+        left: 50,
+        top: 10,
+        backgroundColor: "#00a0ff",
+        borderWidth: 1,
+        borderColor: '#000000',
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    badgeText: {
+        color: '#ffff',
+        fontWeight: '800',
+        fontSize: 12
+    }
+})
+
+export default styles
