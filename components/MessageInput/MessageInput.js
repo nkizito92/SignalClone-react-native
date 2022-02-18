79f426c38f715662a35b397f8e667a6e7ab660e8
nkizito92 <nkizito57@gmail.com>
2022-02-17 22:03:59 -0600

    MessageInput component has been added

---
+++ components/MessageInput/MessageInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const MessageInput = () => {
+    return (
+        <View>
+            <Text> messageInput</Text>
+        </View>
+
+    )
+}
+
+export default MessageInput
