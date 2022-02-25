81b9e36a41824c7599f71bf3fd50dc97103e646c
nkizito92 <nkizito57@gmail.com>
2022-02-24 22:30:45 -0600

    imported react-native-vector modules for special icons

--- components/MessageInput/MessageInput.js
+++ components/MessageInput/MessageInput.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { View, Text, TextInput } from 'react-native'
 import styles from './styles'
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+import Feather from 'react-native-vector-icons/Feather'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 const MessageInput = () => {
     return (
         <View style={styles.root}>
             <View style={styles.inputContainer}>
-                <TextInput style={styles.textInput} />
+                <SimpleLineIcons name="emotsmile" size={24} color="grey" style={styles.icon} />
+                <TextInput style={styles.textInput}
+                placeholder="Signal Message..." />
+                <Feather name="camera" size={24} color="grey" style={styles.icon} />
+                <MaterialCommunityIcons name="microphone-outline" size={24} color="grey" style={styles.icon} />
             </View>
             <View style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>+</Text>
+                <AntDesign name="plus" size={24} color="white" style={styles.icon} />
             </View>
         </View>
     )
