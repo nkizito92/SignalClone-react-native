d7bfe630bae7913f85eb5fccb8321ddeadfa8d24
nkizito92 <nkizito57@gmail.com>
2022-03-04 23:48:22 -0600

    set position to absolute for chatMessages components

--- components/chatMessages/styles.js
+++ components/chatMessages/styles.js
@@ -2,6 +2,8 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
+        position: 'absolute',
+        zIndex: 20 ,
         flexDirection: 'row',
         padding: 10,
         backgroundColor: "#2f285f",
